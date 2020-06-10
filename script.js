20a0c1a91128522b4e35cb44e5f0a20bca386844
ciurca <raduciurca@gmail.com>
2020-06-10 18:15:14 +0300

    fist commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// @raduciurca Github project aggregator
+$.getJSON("https://spreadsheets.google.com/feeds/list/15Qd-UuHy5Ze8zSmGaB9yv2BEpWiKAnAU3DM6BwTtxLY/od6/public/full?alt=json", data => {
+		data.feed.entry.forEach(e => {
+				var temp = document.getElementById('templateCard');
+				var clon = temp.content.cloneNode(true);
+				clon.getElementById('titleCard').innerHTML = e['gsx$title']['$t'];
+				clon.getElementById('subtitleCard').innerHTML = e['gsx$subtitle']['$t'];
+				clon.getElementById('footerCard').innerHTML += `<a href='${e['gsx$github']['$t']}' class="card-footer-item" target="_blank" id="linkId">Github</a>`
+				if (e['gsx$site']['$t'] != ''){
+						clon.getElementById('footerCard').innerHTML += `<a href='${e['gsx$site']['$t']}' class="card-footer-item" target="_blank" id="linkId">Site</a>`
+				}
+				document.getElementById("cardsList").appendChild(clon);
+});
+});
